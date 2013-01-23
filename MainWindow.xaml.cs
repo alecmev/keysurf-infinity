595e6bba60ce52fe294296b3426590f7a40d15b4
Olegs Jeremejevs <olegs@jeremejevs.com>
2013-01-23 22:30:22 +0000

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Microsoft.Win32;
+
+namespace BrightCerulean.KeySurfInfinity
+{
+    public partial class MainWindow : Window
+    {
+        private delegate void AppendStatusDelegate(string text, bool newLine);
+
+        private static MainWindow _instance;
+        public static MainWindow Instance
+        {
+            get { return _instance; }
+        }
+
+        private Worker _worker = new Worker();
+        private Thread _workerThread;
+
+        private NotifyIcon _tray = new NotifyIcon();
+
+        private string _username = "";
+        public string Username
+        {
+            get { return _username; }
+        }
+
+        private string _password = "";
+        public string Password
+        {
+            get { return _password; }
+        }
+
+        private string _filePath;
+
+        public MainWindow()
+        {
+            _instance = this;
+
+            InitializeComponent();
+
+            _filePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "KeySurfInfinity.txt");
+            FileStream tmpStream = new FileStream(_filePath, FileMode.OpenOrCreate);
+            StreamReader tmpReader = new StreamReader(tmpStream);
+            string tmpString = tmpReader.ReadToEnd().Trim();
+            string[] tmpLines = tmpString.Split('\n');
+
+            if (tmpLines.Length == 2)
+            {
+                _username = tmpLines[0].Trim();
+                username.Text = _username;
+                _password = tmpLines[1].Trim();
+                password.Password = _password;
+            }
+
+            tmpStream.Close();
+
+            SystemEvents.PowerModeChanged += PowerModeChanged;
+            _tray.Icon = new Icon(Assembly.GetExecutingAssembly().GetManifestResourceStream("BrightCerulean.KeySurfInfinity.KeySurfInfinity.ico"));
+            _tray.MouseDoubleClick += TrayMouseDoubleClick;
+            var tmpMenuItem = new System.Windows.Forms.MenuItem();
+            tmpMenuItem.Index = 0;
+            tmpMenuItem.Text = "Quit";
+            tmpMenuItem.Click += new EventHandler(QuitClick);
+            _tray.ContextMenu = new System.Windows.Forms.ContextMenu(new System.Windows.Forms.MenuItem[] { tmpMenuItem });
+            _workerThread = new Thread(_worker.Work) { IsBackground = true };
+            _workerThread.Start();
+        }
+
+        private void QuitClick(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            if (e.Mode == PowerModes.Resume)
+                Reload();
+        }
+
+        private void Reload()
+        {
+            _worker.Stop();
+            _worker = new Worker();
+            _workerThread = new Thread(_worker.Work) { IsBackground = true };
+            _workerThread.Start();
+        }
+
+        private void AppendStatus(string text, bool newLine)
+        {
+            while (status.Text.Length > 2048)
+            {
+                var newLineIndex = status.Text.IndexOf('\n');
+                status.Text = status.Text.Remove(0, newLineIndex != -1 ? newLineIndex + 1 : status.Text.Length);
+            }
+
+            var tmpTime = "[" + DateTime.Now.ToString("HH:mm:ss") + "] ";
+            status.Text += (status.Text.Length > 0 ? (newLine ? "\n" + tmpTime : " - ") : "" + tmpTime) + text;
+
+            if (!status.IsMouseOver)
+                status.ScrollToEnd();
+        }
+
+        public void AppendStatusSafe(string text, bool newLine = true)
+        {
+            Dispatcher.BeginInvoke(
+                new AppendStatusDelegate(AppendStatus),
+                DispatcherPriority.Normal,
+                new object[] { text, newLine });
+        }
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _workerThread.Abort();
+        }
+
+        private void TrayMouseDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Normal;
+        }
+
+        private void WindowStateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == System.Windows.WindowState.Minimized)
+                this.ShowInTaskbar = false;
+            else if (this.WindowState == System.Windows.WindowState.Normal)
+                this.ShowInTaskbar = true;
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            _tray.Visible = true;
+            if (_username != "" && _password != "")
+                this.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void PingClick(object sender, RoutedEventArgs e)
+        {
+            Reload();
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            _username = username.Text;
+            _password = password.Password;
+
+            FileStream tmpStream = new FileStream(_filePath, FileMode.Create);
+            StreamWriter tmpWriter = new StreamWriter(tmpStream);
+            tmpWriter.WriteLine(_username);
+            tmpWriter.Write(_password);
+            tmpWriter.Close();
+            tmpStream.Close();
+
+            Reload();
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace BrightCerulean.KeySurfInfinity
+{
+    public class Worker
+    {
+        private volatile bool _stop = false;
+        private bool _success = false;
+
+        public void Work()
+        {
+            while (!_stop)
+            {
+                try
+                {
+                    if (IsEthernetConnected())
+                    {
+                        var tmpRequest = WebRequest.Create("http://myfajoarco.lv/keysurf/ping/") as HttpWebRequest;
+                        tmpRequest.Proxy = null;
+                        ThreadPool.RegisterWaitForSingleObject(tmpRequest.BeginGetResponse(new AsyncCallback(ResponseCallback), tmpRequest).AsyncWaitHandle, new WaitOrTimerCallback(TimeoutCallback), tmpRequest, 4000, true);
+                    }
+                    else
+                    {
+                        _success = false;
+                        MainWindow.Instance.AppendStatusSafe("NO CABLE");
+                    }
+                }
+                catch
+                {
+                    _success = false;
+                    MainWindow.Instance.AppendStatusSafe("ERROR A");
+                }
+
+                Thread.Sleep(4000);
+
+                if (_success)
+                {
+                    _success = false;
+                    Thread.Sleep(56000);
+                }
+            }
+        }
+
+        private void ResponseCallback(IAsyncResult asynchronousResult)
+        {
+            try
+            {
+                if (!asynchronousResult.IsCompleted || asynchronousResult.AsyncState == null)
+                    return;
+
+                MainWindow.Instance.AppendStatusSafe("CHECKING KEYSURF");
+                var tmpStream = (asynchronousResult.AsyncState as HttpWebRequest).EndGetResponse(asynchronousResult).GetResponseStream();
+                var tmpResponse = new StreamReader(tmpStream).ReadToEnd();
+                tmpStream.Close();
+
+                if (tmpResponse != "OK")
+                {
+                    MainWindow.Instance.AppendStatusSafe("SUBMITTING LOGIN INFORMATION", false);
+                    string tmpFormData = "username=" + System.Uri.EscapeUriString(MainWindow.Instance.Username) + "&password=" + System.Uri.EscapeUriString(MainWindow.Instance.Password) + "&original_url=%2Fwelcome.asp&login=Sign%20in";
+
+                    var tmpRequest = WebRequest.Create("http://login.keycom.co.uk:8080/goform/HtmlLoginRequest") as HttpWebRequest;
+                    tmpRequest.Method = "POST";
+                    tmpRequest.Timeout = 4000;
+                    tmpRequest.ContentType = "application/x-www-form-urlencoded";
+                    tmpRequest.ContentLength = tmpFormData.Length;
+                    tmpRequest.Headers.Add("Cache-Control", "max-age=0");
+                    tmpRequest.Headers.Add("Origin", "http://login.keycom.co.uk:8080");
+                    tmpRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1";
+                    tmpRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                    tmpRequest.Referer = "http://login.keycom.co.uk:8080/index.asp";
+                    tmpRequest.Headers.Add("Accept-Encoding", "gzip,deflate,sdch");
+                    tmpRequest.Headers.Add("Accept-Language", "en-US,en;q=0.8");
+                    tmpRequest.Headers.Add("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
+
+                    var tmpWriter = new StreamWriter(tmpRequest.GetRequestStream());
+                    tmpWriter.Write(tmpFormData);
+                    tmpWriter.Close();
+
+                    _success = false;
+                    MainWindow.Instance.AppendStatusSafe("SUCCESS", false);
+                }
+                else
+                {
+                    _success = true;
+                    MainWindow.Instance.AppendStatusSafe("EVERYTHING IS FINE", false);
+                }
+            }
+            catch
+            {
+                _success = false;
+                MainWindow.Instance.AppendStatusSafe("FAIL", false);
+            }
+        }
+
+        private void TimeoutCallback(object state, bool timedOut)
+        {
+            try
+            {
+                if (timedOut)
+                {
+                    var tmpRequest = state as HttpWebRequest;
+
+                    if (tmpRequest != null)
+                        tmpRequest.Abort();
+
+                    _success = false;
+                    MainWindow.Instance.AppendStatusSafe("NO INTERNET CONNECTION");
+                }
+            }
+            catch
+            {
+                _success = false;
+                MainWindow.Instance.AppendStatusSafe("ERROR B");
+            }
+        }
+
+        public void Stop()
+        {
+            _stop = true;
+        }
+
+        private bool IsEthernetConnected()
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+                return false;
+
+            foreach (NetworkInterface tmpNetworkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if ((tmpNetworkInterface.OperationalStatus != OperationalStatus.Up) ||
+                    (tmpNetworkInterface.NetworkInterfaceType == NetworkInterfaceType.Loopback) ||
+                    (tmpNetworkInterface.NetworkInterfaceType == NetworkInterfaceType.Tunnel))
+                    continue;
+
+                if (tmpNetworkInterface.Speed <= 0)
+                    continue;
+
+                if ((tmpNetworkInterface.NetworkInterfaceType != NetworkInterfaceType.Ethernet) &&
+                    (tmpNetworkInterface.NetworkInterfaceType != NetworkInterfaceType.Ethernet3Megabit) &&
+                    (tmpNetworkInterface.NetworkInterfaceType != NetworkInterfaceType.FastEthernetFx) &&
+                    (tmpNetworkInterface.NetworkInterfaceType != NetworkInterfaceType.FastEthernetT) &&
+                    (tmpNetworkInterface.NetworkInterfaceType != NetworkInterfaceType.GigabitEthernet))
+                    continue;
+
+                if ((tmpNetworkInterface.Description.IndexOf("virtual", StringComparison.OrdinalIgnoreCase) >= 0) ||
+                    (tmpNetworkInterface.Description.IndexOf("hamachi", StringComparison.OrdinalIgnoreCase) >= 0) ||
+                    (tmpNetworkInterface.Description.IndexOf("teamviewer", StringComparison.OrdinalIgnoreCase) >= 0) ||
+                    (tmpNetworkInterface.Description.IndexOf("vpn", StringComparison.OrdinalIgnoreCase) >= 0) ||
+                    (tmpNetworkInterface.Description.IndexOf("loopback", StringComparison.OrdinalIgnoreCase) >= 0))
+                    continue;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
